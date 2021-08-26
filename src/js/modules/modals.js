a31469b283f667df219bbf3459a59cea7fc24cc1
Svetlana Galygina <svetlanka.woman@gmail.com>
2021-08-26 15:48:47 +0300

    init project

---
+++ src/js/modules/modals.js
@@ -0,0 +1,9 @@
+const modals = () => {
+   const btnPopupEngineer = document.querySelector('.popup_engineer_btn'),
+         popupEngineer = document.querySelector('.popup_engineer');
+   btnPopupEngineer.addEventListener('click', () => {
+      
+   });
+};
+
+export default modals;
