9c2a4ff4884b45af1144dc118525638c751340e9
Svetlana Galygina <svetlanka.woman@gmail.com>
2021-09-09 10:44:34 +0300

    task 6, part 1 - Calculator

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,15 +1,25 @@
+import { bind } from "core-js/core/function";
+
 const modals = () => {
-  function bindModal(triggerSelector, modalSelector, closeSelector) {  
+  function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {  
     const trigger = document.querySelectorAll(triggerSelector),
           modal = document.querySelector(modalSelector),
-          close = document.querySelector(closeSelector);
+          close = document.querySelector(closeSelector),
+          windows = document.querySelectorAll('[data-modal]');
 
+    function allWindowsHide() {
+      windows.forEach(item => {
+        item.style.display = 'none';
+      });
+    }
     
     trigger.forEach(item => {
       item.addEventListener('click', (e) => {
         if (e.target) {
           e.preventDefault();
         }
+
+        allWindowsHide();
   
         modal.style.display = "block";
         document.body.style.overflow = "hidden";
@@ -18,13 +28,15 @@ const modals = () => {
     });
 
     close.addEventListener('click', () => {
+      allWindowsHide();
       modal.style.display = "none";
       document.body.style.overflow = "";
       //document.body.classList.remove('modal-open'); //class from bootstrap 
     });
 
     modal.addEventListener('click', (e) => {
-      if (e.target === modal) {
+      if (e.target === modal && closeClickOverlay) {
+        allWindowsHide();
         modal.style.display = "none";
         document.body.style.overflow = "";
         //document.body.classList.remove('modal-open'); //class from bootstrap
@@ -41,6 +53,8 @@ const modals = () => {
 
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
   bindModal('.phone_link', '.popup', '.popup .popup_close');
+  bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close', false);
+  bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close');
 
   showModalByTime('.popup');
 
