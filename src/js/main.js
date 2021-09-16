85f2540ff050226b59c7790cd5a8c9384bd40fbf
Svetlana Galygina <svetlanka.woman@gmail.com>
2021-09-16 21:32:15 +0300

    task 8 - timer

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import modals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
 
 window.addEventListener('DOMContentLoaded', () => {
   "use strict";
@@ -11,6 +12,7 @@ window.addEventListener('DOMContentLoaded', () => {
     form: 0,
     type: 'tree'
   };
+  let deadLine = '2021-12-25';
 
   changeModalState(modalState);
 
@@ -19,4 +21,5 @@ window.addEventListener('DOMContentLoaded', () => {
   tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
   tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline');
   forms(modalState);
+  timer('#timer', deadLine);
 });
---
+++ src/js/modules/timer.js
@@ -0,0 +1,60 @@
+const timer = (timerSelector, deadLine) => {
+  const addZero = (num) => {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  };
+
+  const getTimeRemaining = (endTime) => {
+    const t = Date.parse(endTime) - Date.parse(new Date()),
+          days = Math.floor(t / (1000 * 60 * 60 * 24)),
+          hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+          minutes = Math.floor((t / (1000 * 60)) % 60),
+          seconds = Math.floor((t / 1000) % 60);
+    
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+  };
+
+  function setClock(selector, endTime) {
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = document.querySelector('#seconds'),
+          timeInterval = setInterval(updateClock, 1000);
+    
+    updateClock();   
+
+    function updateClock() {
+      const t = getTimeRemaining(endTime);
+
+      days.textContent = addZero(t.days);
+      hours.textContent = addZero(t.hours);
+      minutes.textContent = addZero(t.minutes);
+      seconds.textContent = addZero(t.seconds);
+
+      if (t.total <= 0) {
+        days.textContent = "00";
+        hours.textContent = "00";
+        minutes.textContent = "00";
+        seconds.textContent = "00";
+
+        clearInterval(timeInterval);
+      }
+    }
+    
+    updateClock();
+  }
+
+  setClock(timerSelector, deadLine);
+};
+
+export default timer;
