5b1cf10f4347de8e3fb3a3f37210b5ae256fe525
Svetlana Galygina <svetlanka.woman@gmail.com>
2021-09-20 13:41:01 +0300

    change dist to docs

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
-const dist = "./dist/";
+// const dist = "./dist/";
+const dist = "./docs/";
 // const dist = "../../../../Program Files/OpenServer/domains/test/";
 
 gulp.task("copy-html", () => {
